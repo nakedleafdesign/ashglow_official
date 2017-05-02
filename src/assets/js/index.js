d6b87cd098719157365bb011198f6ac87252af1c
阪西功介 <nakedleafdesign@gmail.com>
2017-05-03 07:49:52 +0900

    [modify] インスタグラムslider実装

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,7 +1,7 @@
 var activeFlag = 'is-active';
 
 $(function () {
-  $( '.js-youtube-slider' ).sliderPro({
+  $('.js-youtube-slider').sliderPro({
     width: 320,
     arrows: true,
     buttons: false,
@@ -9,4 +9,40 @@ $(function () {
     visibleSize: '100%',
     imageScaleMode : 'contain',
   });
+
+
+
+  $(".js-instagram-slider-item-wrap").text("Instagram timeline loading...");
+  $.ajax({
+    url: "https://api.instagram.com/v1/users/self/media/recent",
+    data: {access_token: "4593572693.66f42ee.85db8b1969534f858c3049bdca2b5931"},
+    dataType: "jsonp",
+    error: function (jqXHR, textStatus, errorThrown) {
+      $(".js-instagram-slider-item-wrap").text(textStatus);
+    },
+    success: function (data, textStatus, jqXHR) {
+      $(".js-instagram-slider-item-wrap").text("");
+      for (var i = 0, length = 12; i < length; i++) {
+        var d = data.data[i];
+        $(".js-instagram-slider-item-wrap").append(
+            $("<div>").addClass("sp-slide").append($("<a>").attr("href", d.link).attr("target", "_blank").append($("<img>").addClass("sp-image").attr("src", d.images.thumbnail.url))));
+      }
+
+      // @ 読み込み完了後にスライダーを起動
+      // ------------------------------
+
+      $('.js-instagram-slider').sliderPro({
+        width: 180,
+        arrows: true,
+        buttons: false,
+        slideDistance:15,
+        visibleSize: '100%',
+        imageScaleMode : 'contain',
+      });
+    }
+  });
+
+
+
+
 });
