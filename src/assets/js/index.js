e4a2c233eb2495f9bb238b82814570bd4533d31c
阪西功介 <nakedleafdesign@gmail.com>
2017-05-03 07:20:33 +0900

    [add] [modify] youtube slider 実装

---
+++ src/assets/js/index.js
@@ -0,0 +1,12 @@
+var activeFlag = 'is-active';
+
+$(function () {
+  $( '.js-youtube-slider' ).sliderPro({
+    width: 320,
+    arrows: true,
+    buttons: false,
+    slideDistance:20,
+    visibleSize: '100%',
+    imageScaleMode : 'contain',
+  });
+});
