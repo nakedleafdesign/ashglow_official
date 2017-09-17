ee83a72d0f9c9d10a8165f7d468fc5b5523c4a2f
阪西功介 <nakedleafdesign@gmail.com>
2017-09-18 04:09:55 +0900

    wordpressインストール home index single など必要最低限のファイルを準備

---
+++ src/php/sidebar.php
@@ -0,0 +1,22 @@
+<section class="block-secondary">
+  <aside class="side-module side-bnr-group">
+    <h2 class="side-heading01">SIDE BNR MODULE</h2>
+    <div class="side-bnr-item"><a href="#"><img src="http://placehold.jp/240x80.png" alt=""></a></div>
+    <div class="side-bnr-item"><a href="#"><img src="http://placehold.jp/240x80.png" alt=""></a></div>
+  </aside>
+  <aside class="side-module side-bnr-group">
+    <h2 class="side-heading01">SIDE BNR MODULE</h2>
+    <div class="side-bnr-item--sm-col1"><a href="#"><img src="http://placehold.jp/240x80.png" alt=""></a></div>
+    <div class="side-bnr-item--sm-col1"><a href="#"><img src="http://placehold.jp/240x80.png" alt=""></a></div>
+  </aside>
+  <aside class="side-module side-list-group">
+    <h2 class="side-heading01">SIDE LIST MODULE</h2>
+    <ul class="side-list-body">
+      <li class="side-list-item"><a href="#">サイドリスト</a></li>
+      <li class="side-list-item is-current">サイドリスト</li>
+      <li class="side-list-item"><a href="#">サイドリスト</a></li>
+      <li class="side-list-item"><a href="#">サイドリスト</a></li>
+      <li class="side-list-item"><a href="#">サイドリスト</a></li>
+    </ul>
+  </aside>
+</section>
